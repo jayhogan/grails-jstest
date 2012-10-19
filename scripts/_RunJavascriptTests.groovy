7941a2a1414fac303b844a2eb159ec482f19831a
Jay Hogan <seeker136@gmail.com>
2012-10-19 07:36:09 -0400

    Add Jasmine runner

---
+++ scripts/_RunJavascriptTests.groovy
@@ -0,0 +1,44 @@
+def testsDir = "${basedir}/test/js"
+def targetDir = "${basedir}/target/test-reports/plain"
+def testResourcesDir = "test/resources/lib"
+
+target(runJsTests: 'Runs Jasmine Tests') {
+    event("StatusUpdate", ["Starting Jasmine test phase"])
+
+    def specsToRun = []
+    new File(testsDir).eachFileRecurse {
+        if (it.name.endsWith(".html")) {
+            specsToRun << it
+        }
+    }
+
+    def startTime = new Date()
+    def failures = 0
+    def buildPassed = true
+    specsToRun.each { File testFile ->
+        println "Running ${testFile.name}..."
+        def outputFile = "${targetDir}/TEST-${testFile.name.replace('-', '').replace('.html', '.xml')}"
+        def ant = new AntBuilder()
+        ant.exec(outputproperty: "cmdOut", errorproperty: "cmdErr", resultproperty: "cmdExit", failonerror: "false", executable: "/usr/bin/env") {
+            arg(line: "DISPLAY=:1")
+            arg(line: "/usr/local/bin/phantomjs")
+            arg(line: "${testResourcesDir}/phantomjs-jasmine-runner.js")
+            arg(line: "${testFile.canonicalPath}")
+        }
+        if (ant.project.properties.cmdExit != "0") {
+            buildPassed = false
+            println "\tFAILED!"
+            failures++
+        } else {
+            println "\tPASSED"
+        }
+        new File(outputFile).write(ant.project.properties.cmdOut)
+    }
+
+    println "-------------------------------------------------------"
+    println "Jasmine Tests Completed in ${new Date().time - startTime.time}ms"
+    def msg = "Jasmine Tests ${buildPassed ? "PASSED" : "FAILED"} - view reports in ${targetDir}"
+    println "-------------------------------------------------------"
+    event("StatusFinal", [msg])
+    return buildPassed ? 0 : 1
+}
